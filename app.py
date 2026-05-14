6ab40c4ed4655ba3d6682c3b8981fc6a6023effc
Rohit S <rohit254253@gmail.com>
2026-05-13 17:38:10 -0700

    Added dashboard, hashing and ReadMe

--- app.py
+++ app.py
@@ -1,41 +1,60 @@
-from flask import Flask, request, redirect, render_template, session
+from flask import Flask, render_template, request, redirect, session
 import sqlite3
-import hashlib
-
+from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
+
 app.secret_key = "secret123"
 
-# DB create
+
+# DATABASE CREATE
 def init_db():
-    conn = sqlite3.connect("database.db")
+
+    conn = sqlite3.connect('database.db')
+
     cur = conn.cursor()
+
     cur.execute("""
     CREATE TABLE IF NOT EXISTS users(
+
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT,
         password TEXT
+
     )
     """)
+
     conn.commit()
     conn.close()
 
+
 init_db()
 
-# Home
+
+# HOME
 @app.route('/')
 def home():
+
     return redirect('/login')
 
-# Register
+
+# REGISTER
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = hashlib.sha256(request.form['password'].encode()).hexdigest()
 
-        conn = sqlite3.connect("database.db")
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = generate_password_hash(request.form['password'])
+
+        conn = sqlite3.connect('database.db')
+
         cur = conn.cursor()
-        cur.execute("INSERT INTO users(username, password) VALUES (?,?)", (username, password))
+
+        cur.execute(
+            "INSERT INTO users(username, password) VALUES(?, ?)",
+            (username, password)
+        )
+
         conn.commit()
         conn.close()
 
@@ -43,39 +62,66 @@ def register():
 
     return render_template('register.html')
 
-# Login
+
+# LOGIN
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = hashlib.sha256(request.form['password'].encode()).hexdigest()
 
-        conn = sqlite3.connect("database.db")
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = sqlite3.connect('database.db')
+
         cur = conn.cursor()
-        cur.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+
+        cur.execute(
+            "SELECT * FROM users WHERE username=? ",
+            (username, )
+        )
+
         user = cur.fetchone()
+
         conn.close()
 
-        if user:
+        if user and check_password_hash(user[2], password):
+
             session['user'] = username
+
             return redirect('/dashboard')
+
         else:
-            return "Invalid Credentials"
+
+            return "Invalid Username or Password"
 
     return render_template('login.html')
 
-# Dashboard
+
+# DASHBOARD
 @app.route('/dashboard')
 def dashboard():
+
     if 'user' in session:
-        return f"Welcome {session['user']}"
+
+        return render_template(
+            'dashboard.html',
+            username=session['user']
+        )
+
     return redirect('/login')
 
-# Logout
+
+# LOGOUT
 @app.route('/logout')
 def logout():
+
     session.pop('user', None)
+
     return redirect('/login')
 
-if __name__ == '__main__':
+
+# RUN APP
+if __name__ == "__main__":
+
     app.run(debug=True)
